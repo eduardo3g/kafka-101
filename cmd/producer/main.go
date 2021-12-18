b7facd9595c3095239c021ebda60ad08b7e8ecb3
eduardo3g <eduardosbrasil10@gmail.com>
2021-12-17 21:36:47 -0300

    feat: receive Delivery Report synchronously

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
+	"fmt"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Failed to send message")
+	} else {
+		fmt.Println("Message sent successfully:", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -24,14 +35,14 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
